959cc4857b67a01c14df8c78b180f59f4dfda833
EggsyOnCode <farzansaqib2003@gmail.com>
2024-07-29 02:36:35 +0500

    added func for PlacingMarketOrder

--- core/orderbook.go
+++ core/orderbook.go
@@ -32,10 +32,18 @@ func NewOrder(size int64, bid bool, price float64) *Order {
 	}
 }
 
+func (o *Order) TotalPrice() float64 {
+	return float64(o.Size * int64(o.Price))
+}
+
 func (o *Order) String() string {
 	t := time.Unix(o.Timestamp, 0)
 	format := t.Format("2006-01-02 15:04:05")
 	return fmt.Sprintf("Order{Size: %d, Timestamp: %v, Bid: %v}", o.Size, format, o.Bid)
+}
+
+func (o *Order) IsFilled() bool {
+	return o.Size == 0
 }
 
 type Limit struct {
@@ -61,11 +69,16 @@ type OrderBook struct {
 	// price levels to limit
 	AsksMap map[float64]*Limit
 	BidsMap map[float64]*Limit
+
+	totalBidVolume float64
+	totalAskVolume float64
 }
 
 func NewOrderBook() *OrderBook {
 	return &OrderBook{
-		Asks:    avl.New[float64, *Limit](g.Greater[float64]),
+		// asks are sorted in ascending order : lowest ask first
+		Asks: avl.New[float64, *Limit](g.Less[float64]),
+		// bids are sorted in descending order : highest bid first
 		Bids:    avl.New[float64, *Limit](g.Greater[float64]),
 		AsksMap: make(map[float64]*Limit),
 		BidsMap: make(map[float64]*Limit),
@@ -84,43 +97,116 @@ func (l *Limit) RemoveOrders(orders []*Order) {
 	}
 }
 
-// This is for placing limit orders only
-func (ob *OrderBook) PlaceOrder(price float64, o *Order) []Match {
-	// find matches and fill as much as possible
-	// TODO: Matching logic
+func (l *Limit) Fill(o *Order) []Match {
+	var matches []Match
+	stop := false
 
-	// add the order to a limit bucket ACT its price level
-	if o.Size > 0.0 {
-		ob.add(price, o)
-	}
+	l.Orders.Each(func(key int64, order *Order) {
+		if stop {
+			return
+		}
 
-	// returns the matches
-	return []Match{}
+		match := l.fillOrder(o, order)
+		matches = append(matches, match)
+
+		l.TotalVolume -= match.SizeFilled
+
+		if o.IsFilled() {
+			stop = true
+		}
+	})
+
+	return matches
 }
 
-func (ob *OrderBook) add(price float64, o *Order) {
+func (l *Limit) fillOrder(o, order *Order) Match {
+	var (
+		sizeFilled float64
+		ask        *Order
+		bid        *Order
+	)
+
+	if o.Bid {
+		// if the order is a bid, then the order is a bid and the limit order is an ask
+		ask = order
+		bid = o
+	} else {
+		// if the order is an ask, then the order is an ask and the limit order is a bid
+		ask = o
+		bid = order
+	}
+
+	if o.Size >= order.Size {
+		o.Size -= order.Size
+		sizeFilled = float64(order.Size)
+		order.Size = 0
+	} else {
+		order.Size -= o.Size
+		sizeFilled = float64(o.Size)
+		o.Size = 0
+	}
+
+	updated_ask := *ask
+	updated_bid := *bid
+
+	return Match{
+		Ask:        &updated_ask,
+		Bid:        &updated_bid,
+		SizeFilled: sizeFilled,
+		Price:      o.Price * sizeFilled,
+	}
+}
+
+// This is for placing limit orders only
+// IMP : price level of an order could be different from o.size * o.price
+func (ob *OrderBook) PlaceLimitOrder(price float64, o *Order) {
 	var limit *Limit
 
 	if o.Bid {
 		if l, ok := ob.BidsMap[price]; ok {
 			limit = l
 			l.AddOrder(o)
+			ob.totalBidVolume += o.TotalPrice()
 		} else {
 			limit = NewLimit(price)
 			ob.BidsMap[price] = limit
 			limit.AddOrder(o)
 			ob.Bids.Put(price, limit)
+			ob.totalBidVolume += o.TotalPrice()
 		}
 	} else {
 		if l, ok := ob.AsksMap[price]; ok {
 			limit = l
 			l.AddOrder(o)
+			ob.totalAskVolume += o.TotalPrice()
 		} else {
 			limit = NewLimit(price)
 			ob.AsksMap[price] = limit
 			limit.AddOrder(o)
 			ob.Asks.Put(price, limit)
+			ob.totalAskVolume += o.TotalPrice()
 		}
 	}
 
+}
+
+func (ob *OrderBook) PlaceMarketOrder(price float64, o *Order) []Match {
+	var matches []Match
+
+	if o.Bid {
+		if o.TotalPrice() > ob.totalAskVolume {
+			// market order can't be filled
+			panic("Market order can't be filled")
+		}
+		ob.Asks.Each(func(key float64, l *Limit) {
+			// we'll match the order with the asks ; incrementally starting from the lowest ask
+			limitMatches := l.Fill(o)
+			matches = append(matches, limitMatches...)
+			for _, m := range limitMatches {
+				ob.totalAskVolume -= m.Price
+			}
+		})
+	}
+
+	return matches
 }
--- core/orderbook_test.go
+++ core/orderbook_test.go
@@ -49,7 +49,7 @@ func TestLimit(t *testing.T) {
 func TestOrderBook(t *testing.T) {
 	ob := NewOrderBook()
 	order := NewOrder(3, true, 400)
-	ob.PlaceOrder(1300, order)
+	ob.PlaceLimitOrder(1300, order)
 
 	expectedOrder, _ := ob.BidsMap[1300].Orders.Get(order.Timestamp)
 	assert.Equal(t, expectedOrder, order)
@@ -59,23 +59,47 @@ func TestOrderBook(t *testing.T) {
 func TestSortingOfLimitsAndOrders(t *testing.T) {
 	ob := NewOrderBook()
 	orders := []*Order{
-		NewOrder(3, true, 400),
-		NewOrder(3, true, 400),
-		NewOrder(2, true, 300),
-		NewOrder(1, true, 200),
+		NewOrder(3, false, 400),
+		NewOrder(3, false, 400),
+		NewOrder(2, false, 300),
+		NewOrder(1, false, 200),
 	}
 
 	for _, o := range orders {
-		ob.PlaceOrder(float64(o.Size)*o.Price, o)
+		ob.PlaceLimitOrder(float64(o.Size)*o.Price, o)
 	}
 
-	ob.Bids.Each(func(key float64, val *Limit) {
+	ob.Asks.Each(func(key float64, val *Limit) {
 		fmt.Println(key, val)
 	})
 
-	ob.BidsMap[1200].Orders.Each(func(key int64, val *Order) {
+	ob.AsksMap[1200].Orders.Each(func(key int64, val *Order) {
 		fmt.Println(key, val)
 	})
 
-	assert.Equal(t, ob.Bids.Size(), 3)
+	// assert.NotNil(t, nil)
+
+	assert.Equal(t, ob.Asks.Size(), 3)
+}
+
+func TestPlaceMarketOrder(t *testing.T) {
+	ob := NewOrderBook()
+
+	// selling 3 BTC
+	sellOrder := NewOrder(3, false, 400)
+	sellOrder1 := NewOrder(5, false, 300)
+	ob.PlaceLimitOrder(sellOrder.TotalPrice(), sellOrder)
+	ob.PlaceLimitOrder(sellOrder1.TotalPrice(), sellOrder1)
+
+	// buying 5 BTC
+	buyOrder := NewOrder(5, true, 400)
+	matches := ob.PlaceMarketOrder(buyOrder.TotalPrice(), buyOrder)
+	fmt.Printf("Matches: %v\n", matches)
+
+	assert.Equal(t, len(matches), 2)
+	assert.Equal(t, ob.Asks.Size(), 2)
+	assert.Equal(t, ob.totalAskVolume, float64(700))
+	assert.Equal(t, matches[0].Price, float64(1200))
+
+	// assert.NotNil(t, nil)
 }
